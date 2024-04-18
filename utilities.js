7a805c2ee332cc90a109265f815808e97bd6d345
Sagar-rajarapu <150060057+Sagar-rajarapu@users.noreply.github.com>
2024-04-18 14:47:00 +0530

    Create utilities.js

---
+++ utilities.js
@@ -0,0 +1,20 @@
+//utilities.js
+let navbar =`
+<div class="navbar">
+          <a href="index.html">
+            <img src="logo.PNG" class="logo">
+        </a>
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="login.html">Login</a></li>
+            <li><a href="course.html">Course</a></li>
+            <li><a href="updates.html">Updates</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+    
+          </ul>
+        </div>
+`
+fountion loadMenu(){
+document.getElementByID("nav").innerHTML=navbar;
+}
